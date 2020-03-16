dc7241fb85ba2525552ee183bfd377738505c64d
Samuel Silva <samuel_amaro_4@hotmail.com>
2020-03-16 18:11:12 -0300

    inciando a / para ir ao inicio

--- src/main/java/br/com/samorvell/cobranca/CobrancaApplication.java
+++ src/main/java/br/com/samorvell/cobranca/CobrancaApplication.java
@@ -5,7 +5,9 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 
@@ -19,6 +21,14 @@ public class CobrancaApplication {
 	@Bean
 	public LocaleResolver localeResolver() {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+	
+	@Configuration
+	public static class WebMvcConfigurerAdapter{
+		
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/titulos");
+		}
 	}
 		
 }
